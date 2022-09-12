176964db3afcd525cd2737bcac4d15b13723426e
SobirASD <34741911+SobirASD@users.noreply.github.com>
2022-09-12 11:37:30 +0200

    update task 1

--- Task 1/TestConsole/Program.cs
+++ Task 1/TestConsole/Program.cs
@@ -11,22 +11,32 @@ namespace monitor
         static double _processLifeTime;
         static int _frequency;
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)  // async Task Main
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            SetArguments(args);
+            if (!SetArguments(args)) return;
 
-            Task.Run(ProcessChecker);
+            // добавить CancellationToken 
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
 
-            while (true)
+            // async () => ProcessChecker(cancellationToken);
+            Task task = new Task(async () => ProcessChecker(token), token);
+
+            task.Start();
+
+            while (!token.IsCancellationRequested)
             {
-                if (Console.ReadKey().Key == ConsoleKey.Q) break;
+                if (Console.ReadKey().Key == ConsoleKey.Q)
+                {
+                    cancellationTokenSource.Cancel();
+                }
             }
         }
 
-        private static void ProcessChecker()
+        private static void ProcessChecker(CancellationToken token)
         {
-            while (true)
+            while (!token.IsCancellationRequested)
             {
                 var processes = Process.GetProcessesByName(_processName);
 
@@ -39,19 +49,42 @@ namespace monitor
                 }
 
                 Console.WriteLine(".");
-                Thread.Sleep(_frequency * 100);
+
+                //Thread.Sleep(_frequency * 100);
+
+                // Task.Delay + CancelationToken
+                Task.Delay(2000, token).Start();
             }
         }
 
-        private static void SetArguments(string[] args)
+        private static bool SetArguments(string[] args) // Возвращать bool и если параметры не заданы верно выводить сообщение как правильно запускать и завершить работу
         {
-            if (args.Length < 3) return;
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Not enough details");
+                return false;
+            }
 
             _processName = args[0];
-            _processLifeTime = Convert.ToDouble(args[1]);
-            _frequency = Convert.ToInt32(args[2]) * 60;
 
-            Console.WriteLine($"name = {_processName} \nlife time = {_processLifeTime} min \nsleeping time = {_frequency} min \n");
+            // double.TryParse
+            if (!double.TryParse(args[1], out _processLifeTime))
+            {
+                Console.WriteLine("Cannot convert process life time");
+                return false;
+            };
+
+            // int.TryParse
+            if (!Int32.TryParse(args[2], out _frequency))
+            {
+                Console.WriteLine("Cannot convert frequency time");
+                return false;
+            };
+
+            //_frequency *= 60;
+            Console.WriteLine($"name = {_processName} \nlife time = {_processLifeTime} min \nsleeping time = {_frequency/100} min \n");
+
+            return true;
         }
 
         private static bool CheckLifeTime(Process process)
